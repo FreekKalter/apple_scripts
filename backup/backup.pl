e9e3c2b7641f063c3a548296a3823cda9f933b63
FreekKalter <freek@kalteronline.org>
2011-02-25 13:02:55 +0100

    backup 	-backup heeft nu incremental optie
    import music
    	-werkt nu goed met flac bestande, (omzetten, importeren)
    music_sync
    	-werkt nu goed met omzetten naar mp3 (omzetten, verwijderen uit itunes, verwijderen van disk)
    	- maakt nu map aan op basis van album-artist als deze aanwezig is, waardoor een cd met various artist onder 1 map komen

--- backup/backup.pl
+++ backup/backup.pl
@@ -9,17 +9,26 @@ openlog("backup: ", "pid", "LOG_LOCAL6" );
 
 my $network ='';
 my $restore = '';
+my $incremental = '';
 my $timeout = 0;
-if(!GetOptions("network=s" => \$network, "restore" => \$restore, "timeout:i" => \$timeout)){
-	die "Usage backup.pl [--restore, --network networkname --timeout time]\n";			
+if(!GetOptions("network=s" => \$network, "restore" => \$restore, "timeout:i" => \$timeout, "incremental" => \$incremental)){
+			
+}
+if($restore && $incremental){
+	die "Usage backup.pl [--restore, --network networkname --timeout time]\n";	
 }
 if($timeout){
 	sleep($timeout);
 }
 
 my $client = "/Users/freekkalter/";
-my $server="fkalter\@192.168.2.4:/home/fkalter/macBackup/";
-my $server_external = "fkalter\@kalter.dnsalias.org:/home/fkalter/macBackup/";
+my $server = "fkalter\@192.168.2.4";
+my $server_external = "fkalter\@kalter.dnsalias.org";
+my $folder_root = "/home/fkalter/macBackup/";
+my $folder = $folder_root . "macBackup.0/";
+
+# my $server="fkalter\@192.168.2.4:/home/fkalter/macBackup/";
+
 my $excludes="/Users/freekkalter/Library/Scripts/backup/excludes";
 my $sshkey="/Users/freekkalter/Library/Scripts/backup/id_rsa";
 `/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I` =~ / SSID: (\w*)/;
@@ -30,30 +39,42 @@ my $options = "--verbose --ignore-errors --stats --compress --recursive --times 
 my $backup_options =  "$options --delete --exclude-from=$excludes --delete-excluded";
 my $restore_options = "$options";
 
+my $incremental_command = "rm -rf ${folder_root}macBackup.3 && \\
+									mv ${folder_root}macBackup.2 ${folder_root}macBackup.3 && \\
+									mv ${folder_root}macBackup.1 ${folder_root}macBackup.2 && \\
+									cp -al ${folder_root}macBackup.0 ${folder_root}macBackup.1";
+
 if($network){
 	if($network eq $current_network){
 		if($restore){
-			call_it("$rsync -e \"ssh -i $sshkey\" $restore_options $server $client", "restore");
+			call_it("$rsync -e \"ssh -i $sshkey\" $restore_options $server:$folder $client", "restore");
 		}else{
-			call_it("$rsync -e \"ssh -i $sshkey\" $backup_options $client $server","backup");
+			if($incremental){
+				 print `ssh $server '$incremental_command'`;
+			}
+			call_it("$rsync -e \"ssh -i $sshkey\" $backup_options $client $server:$folder","backup");			
 		}
 	}else{syslog("notice", "Not on $network\n");}
+	
 }else{if($restore){
-	call_it("$rsync -e \"ssh -i $sshkey\" $restore_options $server_external $client", "restore");
+	call_it("$rsync -e \"ssh -i $sshkey\" $restore_options $server_external:$folder $client", "restore");
 }else{
-	call_it("$rsync -e \"ssh -i $sshkey\" $backup_options $client $server_external", "backup");
+	if($incremental){
+		 print `ssh $server_external '$incremental_command'`;
+	}
+	call_it("$rsync -e \"ssh -i $sshkey\" $backup_options $client $server_external:$folder", "backup");
 }}				
 
 closelog();
 
 sub call_it{
 	my ($command, $action) = @_;
-	my $ret_code;
 	syslog("notice", "$action started\n");
-	$ret_code = system($command);
-	if($ret_code == 0){
-		syslog("notice", "$action succeeded\n");
+# 	my $output = `$command`;
+
+	if(my $ret_code = system($command)){
+		syslog("notice", "rsync-$action failed with code: $ret_code\n");
 	}else{
-		syslog("notice", "$action failed with code: $ret_code\n");
+		syslog("notice", "$action succeeded\n");
 	}
 }
